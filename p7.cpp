734337e7c82000be7a32b4ae0f6ff6826098cf09
Liam Shanahan <lpshano@gmail.com>
2024-11-11 17:49:33 +0100

    Implement linear search

---
+++ p7.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> numbers = {20, 500, 10, 5, 100, 1, 50};
+
+
+std::string lin_sort(const int &n) {
+    bool contains = false;
+   for (int i = 0; i < numbers.size(); i++) {
+        if (numbers[i] == n) {
+            contains = true;
+        } 
+    } 
+    
+    if (contains == true) {
+            return "contains ";
+        } else {
+            return "does not contain ";
+        }
+    return "error of some kind";
+}
+
+int main() {
+    int n;
+
+    std::cout << "Hey, I can tell if your number is in the list\n";
+    while (true) {
+        std::cout << "Tell me a number: ";
+    std::cin >> n;
+
+    std::cout << "the vector " << lin_sort(n) << n << "\n";
+    }
+
+    return 0;
+}
+
+
+
+
+/*Objetive 1: implement linear search. 
+*/
