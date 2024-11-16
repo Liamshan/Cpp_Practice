b365489c8cf12f4f74ad558e7ec1df1a7e8009bf
Liam Shanahan <lpshano@gmail.com>
2024-11-16 22:42:27 +0100

    just moving around some notations

---
+++ p103.cpp
@@ -0,0 +1,32 @@
+/*
+final
+one 
+for 
+clout
+let's 
+go
+asd
+fd
+s
+d
+fg
+h
+gdf
+gtrfd
+s
+gf
+
+as
+as
+df
+s
+fas
+df
+
+a
+sdaf
+das
+
+
+is this cheating?
+no cuz I'm actually doing a course that doesn't involve uploading to git. */
