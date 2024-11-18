a05fb8c93759ae603aa0a2703877077285961229
Liam Shanahan <lpshano@gmail.com>
2024-11-18 22:09:06 +0100

    selection sort practice

---
+++ p9_4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+void selec_sort(std::vector<int> &vect);
+
+
+int main() {
+    std::vector<int> numbers {54, 2, 300, 254, 66, 21, 5, 0, 9};
+    std::cout << "Yo, I can selection sort a vector: " << "\n";
+    std::cout << "Here is the sorted vector ";
+
+    selec_sort(numbers);
+
+    // Print out the vector
+    for (int i = 0; i < numbers.size(); i++) {
+        std::cout << numbers[i] << ", ";
+    }
+
+    return 0;
+}
+
+void selec_sort(std::vector<int> &vect) {
+    
+    for (int i = 0; i < vect.size(); i++) {
+        int littlest_guy = i;
+        
+        for (int j = i+1; j < vect.size(); j++) {
+            if (vect[littlest_guy] > vect[j]) {
+                littlest_guy = vect[j];
+            }
+        }
+        //stage    take the little guy, put in the i position. save the i position, put in the littlest guy position. 
+        //swap
+        int stage = vect[i];
+        vect[i] = vect[littlest_guy];
+        vect[littlest_guy] = stage;
+    }
+}
