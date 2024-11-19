c564fd45e8343fe63202cf95d06b26d6d21837b9
Liam Shanahan <lpshano@gmail.com>
2024-11-19 19:34:23 +0100

    Working on bubble sort

---
+++ p10.cpp
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+/*
+We're gonna talk about bubble sort right now. 
+ok, going thru the notes of bubble sort here the example from
+the chatgpt example. so here we start the main lop
+we create a vector array then we call bubble sort. then in the bubblesort 
+function we are initiallizing n to be array.size() then we have a first for
+loop where i hicks up while i is less than n-1. prob becuase you dont want to 
+sort the last one with teh one to the right of it. on
+then what we're doihng is jumping into a j loop. very similar. for j is less also
+less than so j starts at zero aswell and also wants to stay smaller than n-1
+then itterates up. so now we're finally getting into the meat of this code
+an if statement of if arr{j} so 64 is greater than j+1
+std::swap arr[j], arr[j+1]
+
+om thats easy as fuck. not sure why didnt try that initially but fuck it
+i didnt know this std::swap() command. ok I can whip this up real quick.
+*/
