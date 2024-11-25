df66a9a1deed237054f8add3129931dfa4d418d6
Liam Shanahan <lpshano@gmail.com>
2024-11-25 19:46:24 +0100

    just notes and plans for tomorrow's work

---
+++ p101.cpp
@@ -0,0 +1,4 @@
+/*
+So, anyway, that's the plan, tomorrow morning at 6am we'll be starting module 4 lab, and i dont think it's going to be that hard, but you are going to be able to
+learn an insane amount about registers interrupt handling and you'll have plenty of research to do to fully understand all of this, so i'm really excited to get into it
+first thing in the morning*/
---
+++ p102.cpp
@@ -0,0 +1,8 @@
+/*
+anyway, so the goal tomorrow is to work on this stuff from 6am to 10am, 
+then we head into the city to buy ceramics and stuff,
+ then we can get lunch at honest greens around 2 to do some more work, 
+ which will be pretty clutch for getting back into a working routine, 
+ then we will go to that bar we liked for those drinks we like, and then come backt o the campsite for
+ dinner. gonna be dope. 
+ */
